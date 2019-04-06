16dd600e1d381b4195bca36bc7563b8e8c3efe12
Дарья Павлюнина <32895938+daryapav@users.noreply.github.com>
2019-04-06 17:41:59 +0300

    Add files via upload

--- HomeWork1/orm.py
+++ HomeWork1/orm.py
@@ -3,12 +3,12 @@ from mysql.connector import Error
 import itertools
 
 
-default_config = {
- 'user': 'root',
- 'password': 'root',
- 'host': 'localhost',
- 'database': 'mysqldb'
- }
+conn = mysql.connector.connect(user ='root',
+        password ='root',
+        host ='localhost',
+        database ='mysqldb')
+
+
 
 class Field:
     def __init__(self, f_type, required=True, default=None):
@@ -37,7 +37,6 @@ class StringField(Field):
 
 
 
-
 class ModelMeta(type):
     def __new__(mcs, name, bases, namespace):
         if name == 'Model':
@@ -49,31 +48,21 @@ class ModelMeta(type):
         if not hasattr(meta, 'table_name'):
             raise ValueError('table_name is empty')
 
-        # todo mro     //я сделаю
-
+        # mro
+        namespace['_fields'] = {}
+        if bases[0] != Model:
+            for base in bases:
+                for key, field in base._fields.items():
+                    namespace['_fields'][key] = field
+                
         fields = {k: v for k, v in namespace.items()
-                  if isinstance(v, Field)}
-        namespace['_fields'] = fields
+                    if isinstance(v, Field)}
+        for key, field in fields.items():
+            namespace['_fields'][key] = field  
+
         namespace['_table_name'] = meta.table_name
         return super().__new__(mcs, name, bases, namespace)
 
-
-
-
-# def  map_fields_type(t):
-#     if t == int:
-#         return 'INT'
-#     if t == str:
-#         return 'TEXT'
-
-
-# def make_fields_stmt(meta_fields):
-#     fields = []
-#     for k, v in meta_fields.items():
-#         # f_type = map_fields_type(v)
-#         fields.append('{f_name} {f_type}'.format(f_name = k, f_type = v))
-#     # print(', '.join(fields))
-#     return ', '.join(fields)
 
 
 
@@ -89,121 +78,96 @@ class Manage:
             self.fields = owner._fields
         return self
 
+    def input_value(self, input_dict):
+        result = {}
+        for input_key, input_value in input_dict.items():
+            if input_key not in self.fields:
+                raise ValueError ('Incorrect key')
+            input_value = self.fields[input_key].validate(input_value)
+            if type(input_value) == str:
+                result[input_key] = input_value             
+            else: 
+                result[input_key] = str(input_value)
+        return result
+
+
     def create(self, **kwargs):
-        try:
-            conn = mysql.connector.connect(**default_config)
-            cursor = conn.cursor()
-            fields = {}
-            for fields_key, fields_value in kwargs.items():
-                if type(fields_value) == str:
-                    fields[fields_key] = "'" + fields_value + "'" 
-                elif type(fields_value) == int:
-                    fields[fields_key] = str(fields_value)
-                else:
-                    raise ValueError ('Incorrect type')
+        cursor = conn.cursor()
+        result = self.input_value(kwargs)
 
-            cursor.execute('INSERT INTO {table_name} ({fields_key}) VALUES ({fields_value}) '.format(
-                    table_name = self.table_name, 
-                    fields_key = ' '.join(fields.keys()),
-                    fields_value = ' '.join(fields.values())))
-            conn.commit()
+        query = (
+            "INSERT INTO {table_name} ({result_key}) VALUES (%s)".format(
+                table_name = self.table_name,
+                result_key = ' '.join(result.keys())
+            ))
+        args = []
+        for key, value in result.items():
+            args.append(value)
+        cursor.execute(query, args)
 
-        except Error as e:
-            print(e)
+        conn.commit()
+        cursor.close()
 
-        finally:
-            cursor.close()
-            conn.close()
 
     def read(self):
-        try:
-            conn = mysql.connector.connect(**default_config)
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM {table_name}".format(table_name = self.table_name))
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM {table_name}".format(table_name = self.table_name))
+        return cursor.fetchall()
+        cursor.close()
 
-            row = cursor.fetchone()
 
-            while row is not None:
-                print(row)
-                row = cursor.fetchone()
-
-        except Error as e:
-            print(e)
-
-        finally:
-            cursor.close()
-            conn.close()
-    
     def delete(self, **kwargs):
-        try:
-            conn = mysql.connector.connect(**default_config)
-            cursor = conn.cursor()
-            fields = {}
-
-            for fields_key, fields_value in kwargs.items():                   # возможно, это стоит вынести в отдельную функцию 
-                if type(fields_value) == str:                            
-                    fields[fields_key] = "'" + fields_value + "'" 
-                elif type(fields_value) == int:
-                    fields[fields_key] = str(fields_value)
-                else:
-                    raise ValueError ('Incorrect type')
-                    
-            cursor.execute('DELETE FROM {table_name} WHERE {fields_key} = {fields_value}'.format(
-                table_name = self.table_name,
-                fields_key = ' '.join(fields.keys()),
-                fields_value = ' '.join(fields.values())
-            ))
-            conn.commit()
-
-        except Error as e:
-            print(e)
-
-        finally:
-            cursor.close()
-            conn.close()
+        cursor = conn.cursor()
+        result = self.input_value(kwargs)
+       
+        query = (
+           "DELETE FROM {table_name} WHERE {result_key} = %s".format(
+               table_name = self.table_name,
+               result_key = ' '.join(result.keys())
+           ))
+        args = []
+        for key, value in result.items():
+            args.append(value)
+        cursor.execute(query, args)
+    
+        conn.commit()
+        cursor.close()
         
     def update(self, **kwargs):
-        try:
-            conn = mysql.connector.connect(**default_config)
-            cursor = conn.cursor()
-            fields = {}
-            for fields_key, fields_value in kwargs.items():
-                if type(fields_value) == str:
-                    fields[fields_key] = "'" + fields_value + "'" 
-                elif type(fields_value) == int:
-                    fields[fields_key] = str(fields_value)
-                else:
-                    raise ValueError ('Incorrect type')
+        cursor = conn.cursor()
+        result = self.input_value(kwargs)
             
-            if len(fields) == 2:
-                n = len(fields) // 2          
-                i = iter(fields.items())      
-                d1 = dict(itertools.islice(i, n))   
-                d2 = dict(i)
-
-                cursor.execute('UPDATE {table_name} SET {d1_key} = {d1_value} WHERE {d2_key} = {d2_value}'.format(
-                    table_name = self.table_name,
-                    d1_key = ' '.join(d1.keys()),
-                    d1_value = ' '.join(d1.values()),
-                    d2_key = ' '.join(d2.keys()),
-                    d2_value = ' '.join(d2.values())
-                ))
-            else:           #len(fields) == 1
-                cursor.execute('UPDATE {table_name} SET {fields_key} = {fields_value}'.format(
-                    table_name = self.table_name,
-                    fields_key = ' '.join(fields.keys()),
-                    fields_value = ' '.join(fields.values())
-                ))
-        
-
-            conn.commit()
-
-        except Error as e:
-            print(e)
+        if len(result) == 2:
+            n = len(result) // 2          
+            i = iter(result.items())      
+            d1 = dict(itertools.islice(i, n))   
+            d2 = dict(i)
             
-        finally:
-            cursor.close()
-            conn.close()
+            query = ("UPDATE {table_name} SET {d1_key} = %s WHERE {d2_key} = %s".format(
+                table_name = self.table_name,
+                d1_key = ' '.join(d1.keys()),
+                d2_key = ' '.join(d2.keys()),
+            ))
+            args = []
+            for key, value in d1.items():
+                args.append(value)
+            for key, value in d2.items():
+                args.append(value)
+
+            cursor.execute(query, args)
+            
+        else: 
+            query = ("UPDATE {table_name} SET {result_key} = %s".format(
+                table_name = self.table_name,
+                result_key = ' '.join(result.keys()),
+            ))
+            args = []
+            for key, value in result.items():
+                args.append(value)
+            cursor.execute(query, args)   
+
+        conn.commit()
+        cursor.close()
 
 
 
@@ -230,9 +194,11 @@ class User(Model):
         table_name = 'user'
 
 
-# class Man(User):
-#     sex = StringField()
+class Man(User):
+    sex = StringField()
 
+    class Meta:
+        table_name = 'user1'
 
 # user = User(id=1, name='name')
 # User.objects.create(id=1, name='name')
@@ -240,11 +206,10 @@ class User(Model):
 
 # User.objects.update(id=1)  
 
-
-# User.objects.delete(id=5)
+# User.objects.delete(id=16)
 
 # User.objects.delete(name='Pasha')
-# User.objects.delete (name = [1,2,3])
+
 
 # User.objects.filter(id=2).filter(name='petya')
 
@@ -252,7 +217,9 @@ class User(Model):
 # user.save()
 # user.delete()
 
-# User.objects.update(name = 'Dasha', id=9)
-# User.objects.update(name = 'Jenya')
-User.objects.read()
-# User.objects.create(name = 'Alica')
+# User.objects.update(name = 'Dasha', id=15)
+# User.objects.update(name = 'Igor')
+# User.objects.create(name = 'Petya')
+print(User.objects.read())
+# Man.objects.read()
+
